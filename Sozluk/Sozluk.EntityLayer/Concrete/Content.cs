8b5a04f8bc055fe8e648a85f2f4955135f74580c
Guney <guneycin@gmail.com>
2025-05-04 08:46:27 +0300

    Add entity classes for About, Category, Contact, Content, Heading, and Writer
    New classes have been added to the `Sozluk.EntityLayer.Concrete` namespace, representing various entities in the application. The classes include:
    - **About**: Properties for `Id`, `Details1`, `Details2`, `Image1`, and `Image2`.
    - **Category**: Properties for `Id`, `Name`, `Description`, and `Status`.
    - **Contact**: Properties for `Id`, `UserName`, `UserMail`, `Subject`, and `Message`.
    - **Content**: Properties for `Id`, `Value`, and `Date`, with comments for potential future properties.
    - **Heading**: Properties for `Id`, `Name`, and `Date`.
    - **Writer**: Properties for `Id`, `Name`, `Surname`, `Image`, `Mail`, and `Password`.
    
    These additions enhance the data model structure for better management of application entities.

---
+++ Sozluk/Sozluk.EntityLayer/Concrete/Content.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sozluk.EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public DateTime Date { get; set; }
+        //ContentYazar
+        //ContentBaşlık
+    }
+}
---
+++ Sozluk/Sozluk.EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sozluk.EntityLayer.Concrete
+{
+    public class Heading
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
