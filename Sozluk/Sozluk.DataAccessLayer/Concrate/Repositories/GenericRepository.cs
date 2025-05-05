4b358fb70142051b94496e976bebabe440cdfc25
Guney <guneycin@gmail.com>
2025-05-05 14:45:18 +0300

    Implement generic repository pattern for data access
    Introduced a new interface `IRepository<T>` for basic CRUD operations and added a concrete implementation `GenericRepository<T>`. This allows for better abstraction and interaction with the database using a `Context` object, supporting operations to list, insert, delete, and update entities.

---
+++ Sozluk/Sozluk.DataAccessLayer/Concrate/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Sozluk.DataAccessLayer.Abstract;
+
+namespace Sozluk.DataAccessLayer.Concrate.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T p)
+        {
+            _context.Set<T>().Remove(p);
+            _context.SaveChanges();
+        }
+
+        public void Insert(T p)
+        {
+            _context.Set<T>().Add(p);
+            _context.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _context.Set<T>().Where(filter).ToList();
+        }
+
+        public void Update(T p)
+        {
+            _context.Set<T>().Update(p);
+            _context.SaveChanges();
+        }
+    }
+}
