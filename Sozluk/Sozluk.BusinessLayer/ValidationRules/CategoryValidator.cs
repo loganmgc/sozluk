7b429fa0c5a797c5e5e98bf97045b1bcdbf27cc4
Guney <guneycin@gmail.com>
2025-05-06 15:11:58 +0300

    Add FluentValidation and CategoryValidator class
    Updated `Sozluk.BusinessLayer.csproj` to include a reference to `FluentValidation` version `12.0.0`. Added `CategoryValidator` class to define validation rules for the `Category` entity, ensuring proper validation for `Name` and `Description` properties with custom error messages.

---
+++ Sozluk/Sozluk.BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Sozluk.EntityLayer.Concrete;
+
+namespace Sozluk.BusinessLayer.ValidationRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz.")
+                .MinimumLength(3).WithMessage("En az 3 karakter girişi yapın.")
+                .MaximumLength(50).WithMessage("20 karakterden fazla giriş yapamazsınız.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Kategori açıklamasını boş geçemezsiniz.")
+                .Length(5, 200).WithMessage("Açıklama 5-200 karakter arasında olmalıdır.");
+        }
+    }
+}
