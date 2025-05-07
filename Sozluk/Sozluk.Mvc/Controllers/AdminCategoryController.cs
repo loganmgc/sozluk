403f09e3e5e749323075db5f4cfef2db16212cf2
Guney <guneycin@gmail.com>
2025-05-07 09:58:52 +0300

    Add category management functionality
    Updated AdminCategoryController to manage categories, including
    listing and adding new categories with validation. Modified
    Index.cshtml to display categories dynamically and added
    AddCategory.cshtml for category creation with form validation.

--- Sozluk/Sozluk.Mvc/Controllers/AdminCategoryController.cs
+++ Sozluk/Sozluk.Mvc/Controllers/AdminCategoryController.cs
@@ -1,11 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Sozluk.BusinessLayer.Abstract;
+using Sozluk.BusinessLayer.ValidationRules;
+using Sozluk.EntityLayer.Concrete;
 
 namespace Sozluk.Mvc.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        private readonly ICategoryService _cm;
+
+        public AdminCategoryController(ICategoryService cm)
+        {
+            _cm = cm;
+        }
         public IActionResult Index()
         {
+            var categoryValues = _cm.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                _cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            foreach (var item in results.Errors)
+            {
+                ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            }
             return View();
         }
     }
